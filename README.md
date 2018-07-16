155197ddd098f77c7fb820e3a2c3494d1b1199a0
Buck Tower <BTower@slb.com>
2018-07-16 16:57:56 -0500

    More windows setup

--- README.md
+++ README.md
@@ -4,5 +4,20 @@ Going for cross-platform compatability of standard configurations for things lik
 
 ## Setup
 
+1. Install [Git](https://git-scm.com/) if you haven't already
+2. `cd $HOME`
+3. Clone this repo: `git clone git@github.com:bucktower/dotfiles.git`
+
+If you've never used the [command line](https://www.davidbaumgold.com/tutorials/command-line/) before, here's the application you're looking for:
+
+  - Linux: Terminal
+  - Mac: Terminal
+  - Windows: Powershell
+
+*(Markdown doesn't currently support ordered lists starting at an arbitrary number like 4, so just look for your OS and continue the OS-specific setup process)*
+
 ### Windows
 
+4. `cd dotfiles`
+5. `.\WindowsSetup.ps1`
+  - This is a Powershell script that I have written to do all the setup. Go do something outside now while I handle the setup
--- WindowsSetup.ps1
+++ WindowsSetup.ps1
@@ -1,4 +1,32 @@
 # Setup Windows devironment using Powershell
 # Built targeting Windows 10
 
-New-Item -ItemType SymbolicLink -Path $HOME\.vimrc -Value $HOME\dotfiles\editors\.vimrc
+Write-Host 'Beginning Windows setup...'
+
+# INSTALLATION
+# Largely using Scoop (https://scoop.sh) as a package manager. I like it better than Chocolatey
+Write-Host 'Installing Scoop as package manager...'
+Invoke-Expression (new-object net.webclient).downloadstring('https://get.scoop.sh')(new-object net.webclient).downloadstring('https://get.scoop.sh')
+
+Write-Host 'Installing various Scoop packages for general usability...'
+Invoke-Expression 'scoop install calibre-normal discord filezilla flux hexchat libreoffice-fresh slack'
+
+Write-Host 'Installing various Scoop packages for development...'
+Invoke-Expression 'scoop install sudo'
+Invoke-Expression 'sudo scoop install 7zip openssh --global'
+Invoke-Expression 'scoop install curl grep sed less touch'
+Invoke-Expression 'scoop install python ruby go nodejs-lts'
+Invoke-Expression 'scoop install dotnet-sdk'
+Invoke-Expression 'scoop install vim'
+
+Invoke-Expression 'scoop install conemu devdocs insomnia vscode' # Not in main Scoop bucket, but still useful to have
+
+Invoke-Expression 'scoop install autohotkey' # Useful for hotkeys (see below)
+
+# HOTKEYS
+Write-Host 'Setting up hotkeys...'
+Copy-Item ".\hotkeys\windows\swap_ctrl_alt.ahk" "$HOME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" # Swap CTRL with ALT for a more Mac-like feel
+
+# EDITORS
+Write-Host 'Setting up editor configurations...'
+New-Item -ItemType SymbolicLink -Path $HOME\.vimrc -Value $HOME\dotfiles\editors\.vimrc # Symlink ~/.vimrc to our git-tracked vimrc
